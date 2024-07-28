f390018d202584c70ec3eb5903c098868a0ef8af
Eric_2139 <ericpowercosta2005@gmail.com>
2024-07-28 18:30:12 -0300

    implementação da camada de domínio

---
+++ src/main/java/dio/spring_boot/model/News.java
@@ -0,0 +1,8 @@
+package dio.spring_boot.model;
+
+import jakarta.persistence.*;
+
+@Entity(name = "tb_news")
+public class News extends BaseItem{
+
+}
---
+++ src/main/java/dio/spring_boot/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package dio.spring_boot.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import dio.spring_boot.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
