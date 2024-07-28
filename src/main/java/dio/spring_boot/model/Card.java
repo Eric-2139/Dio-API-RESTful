2e9104508b5df3f7b895d1c733b8b118f2e453e5
Eric_2139 <ericpowercosta2005@gmail.com>
2024-07-28 18:38:06 -0300

    consertado o scale e precision

--- src/main/java/dio/spring_boot/model/Card.java
+++ src/main/java/dio/spring_boot/model/Card.java
@@ -14,7 +14,7 @@ public class Card {
     @Column(unique = true)
     private String number;
 
-    @Column(name = "available limit", scale = 13 ,precision = 2)
+    @Column(name = "available limit",precision = 13, scale = 2)
     private BigDecimal limit;
 
     public Long getId() {
